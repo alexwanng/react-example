0012ee0146524b69e2e3989199f32d0ccc08a863
Alex Wang <idea.wangtj@gmail.com>
2020-11-19 23:35:13 +0800

    export const books , import with {}

--- src/a01/booklist.js
+++ src/a01/booklist.js
@@ -2,21 +2,7 @@ import React from "react";
 
 import "./booklist.css";
 
-const books = [
-    {
-        img: "https://m.media-amazon.com/images/I/51QBoRpApkL.jpg",
-        title: "Devolution: A Firsthand Account of the Rainier Sasquatch",
-        author: "Max Brooks"
-    },
-
-    {
-        img: "https://m.media-amazon.com/images/I/51n0VhTxftL._SY346_.jpg",
-        title: "The City We Became",
-        author: "N. K. Jemisin"
-    }
-];
-
-
+import {books} from "./books";
 
 
 function BookList() {
