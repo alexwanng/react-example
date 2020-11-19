306040f507af2b6e682a25fd8191a4d0cdd6ecdc
Alex Wang <idea.wangtj@gmail.com>
2020-11-19 23:39:25 +0800

    export default Book, and import Book from "./book"

--- src/a01/booklist.js
+++ src/a01/booklist.js
@@ -4,6 +4,8 @@ import "./booklist.css";
 
 import {books} from "./books";
 
+import Book from "./book";
+
 
 function BookList() {
   return (
@@ -16,31 +18,5 @@ function BookList() {
   );
 }
 
-const Book = (props) => {
-  const {img, title, author, children} = props;
-  const clickHandler = (e) => {
-      console.log(e.target);
-      console.log(e);
-      alert('hello world');
-  };
-
-  const complexExample = (author) => {
-      console.log(author);
-  };
-
-
-  //console.log(props);
-  return (
-    <article className="book">
-      <img src={img} alt="" />
-      <h1 onClick={() => console.log(title)}>{title}</h1>
-      <h4> {author} </h4>
-        <button type="button" onClick={clickHandler}>Reference example</button>
-        {/** it needs to () => to pass auguments to function **/}
-        <button type='button' onClick={() => complexExample(author)}>complex Example</button>
-      <hr />
-    </article>
-  );
-};
 
 export default () => <BookList />;
